36c85333b3c9aa94b1b13c72995cab336b429d32
Juan Cruz de Urquiza <deurquizajuancruz@gmail.com>
2022-09-15 19:58:24 -0300

    Clase 13

---
+++ clase13/js/operaciones.js
@@ -0,0 +1,15 @@
+/*codigo en javascript*/
+// manera interna
+function saludar() {
+    alert('Hola Mundo dentro de saludar()');
+}
+
+// operadores y cuentas matematicas
+console.log(1+2);
+//variables
+let op1 =prompt('Ingrese el valor de op1: '); //devuelve cadena de string
+let op2 =prompt('Ingrese el valor de op2: ');;
+let res 
+res= (+op1) + parseInt(op2);
+//muestro el resultado por consola
+console.log('Resultado='+res);
